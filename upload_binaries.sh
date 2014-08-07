60e5fe34fa99a92b017e6641f6fad6a62d3d9c12
skiwi <frank_skiwi@hotmail.com>
2014-08-07 18:01:17 +0200

    Fixed issue where the upload binaries script would try to ZIP everything on Travis CI.

--- upload_binaries.sh
+++ upload_binaries.sh
@@ -96,7 +96,7 @@ if [ "$TRAVIS_BRANCH" == "master" ]; then
 
   echo -e "Zipping release\n"
   cd /home/travis/build/${GH_USER}/${GH_REPO}/target/jfx/app
-  zip -rsfT releases.zip /
+  zip -rsfT releases.zip .
   cd /
 
   echo -e "Uploading ZIP\n"
