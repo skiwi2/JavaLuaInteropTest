bc09c1661040d01055a1d934fd2d7c2ff2cc53d6
skiwi <frank_skiwi@hotmail.com>
2014-08-07 12:51:27 +0200

    Added basic layout and controls.

--- src/com/skiwi/javaluainteroptest/JavaLuaInteropTestController.java
+++ src/com/skiwi/javaluainteroptest/JavaLuaInteropTestController.java
@@ -3,7 +3,10 @@ package com.skiwi.javaluainteroptest;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 
 /**
  * FXML Controller class
@@ -11,8 +14,24 @@ import javafx.fxml.Initializable;
  * @author Frank van Heeswijk
  */
 public class JavaLuaInteropTestController implements Initializable {
+    @FXML
+    private TextArea codeTextArea;
+    
+    @FXML
+    private TextField valueTextField;
+    
+    @FXML
+    private TextField resultTextField;
+    
     @Override
     public void initialize(final URL url, final ResourceBundle resourceBundle) {
-        
+        codeTextArea.appendText("-- Always name this function \"applyFunction\"");
+        codeTextArea.appendText(System.lineSeparator());
+        codeTextArea.appendText(System.lineSeparator());
+        codeTextArea.appendText("function applyFunction(value)");
+        codeTextArea.appendText(System.lineSeparator());
+        codeTextArea.appendText("    -- your implementation here");
+        codeTextArea.appendText(System.lineSeparator());
+        codeTextArea.appendText("end");
     }    
 }
